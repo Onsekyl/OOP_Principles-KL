6c3ea66fb9fe27a8935a36061d169f46c7dc3a6f
Onsekyl <kimi.leinolaPC@outlook.com>
2024-08-11 20:46:30 +0300

    Weapon change with mousewheel

--- Assets/Scripts/PlayerWeaponManager.cs
+++ Assets/Scripts/PlayerWeaponManager.cs
@@ -7,34 +7,63 @@ public class PlayerWeaponManager : MonoBehaviour
     [SerializeField]
     private GameObject[] _weapons;
 
+    private int _currentWeaponIndex;
+
     private void Start()
     {
-        SelectWeapon(1);
+        _currentWeaponIndex = 1;
+        SelectWeapon();
     }
 
     private void Update()
     {
         if (Input.GetKey(KeyCode.Alpha1))
         {
-            SelectWeapon(0);
+            _currentWeaponIndex = 0;
+            SelectWeapon();
         }
         else if (Input.GetKey(KeyCode.Alpha2))
         {
-            SelectWeapon(1);
+            _currentWeaponIndex = 1;
+            SelectWeapon();
         }
         else if (Input.GetKey(KeyCode.Alpha3))
         {
-            SelectWeapon(2);
+            _currentWeaponIndex = 2;
+            SelectWeapon();
+        }
+
+        if (Input.mouseScrollDelta.y > 0)
+        {
+            _currentWeaponIndex++;
+
+            if (_currentWeaponIndex >= _weapons.Length)
+            {
+                _currentWeaponIndex = 0;
+            }
+
+            SelectWeapon();
+        }
+        else if(Input.mouseScrollDelta.y < 0)
+        {
+            _currentWeaponIndex--;
+
+            if(_currentWeaponIndex < 0)
+            {
+                _currentWeaponIndex = _weapons.Length - 1;
+            }
+
+            SelectWeapon();
         }
     }
 
-    private void SelectWeapon(int weaponIndex)
+    private void SelectWeapon()
     {
         for (int i = 0; i < _weapons.Length; i++)
         {
             _weapons[i].SetActive(false);
         }
 
-        _weapons[weaponIndex].SetActive(true);
+        _weapons[_currentWeaponIndex].SetActive(true);
     }
 }
