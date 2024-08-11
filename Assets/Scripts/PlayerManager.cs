4757127be31f18e7f01ca77476c0120f7cd5d569
Onsekyl <kimi.leinolaPC@outlook.com>
2024-08-11 12:42:46 +0300

    PlayerManager to handle misc things for player

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Cursor.lockState = CursorLockMode.None;
+            GameManager.Instance.StartMenu();
+        }
+    }
+}
