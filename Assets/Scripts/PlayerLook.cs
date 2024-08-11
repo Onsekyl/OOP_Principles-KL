00b0168589f29bb69af009afa59bab0efbd60df4
Onsekyl <kimi.leinolaPC@outlook.com>
2024-08-11 12:04:13 +0300

    PlayerLook MVP

--- Assets/Scripts/PlayerLook.cs
+++ Assets/Scripts/PlayerLook.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class PlayerLook : MonoBehaviour
 {
+    private Camera _playerCamera;
+
+    private Vector2 _lookDirection;
     private Vector2 _lookInput;
+
     private float _lookInputX;
     private float _lookInputY;
 
@@ -13,7 +17,7 @@ public class PlayerLook : MonoBehaviour
 
     private void Start()
     {
-        
+        _playerCamera = this.gameObject.transform.GetChild(0).GetComponent<Camera>();
     }
 
     private void Update()
@@ -25,9 +29,12 @@ public class PlayerLook : MonoBehaviour
     {
         _lookInputX = Input.GetAxisRaw("Mouse X");
         _lookInputY = Input.GetAxisRaw("Mouse Y");
-
         _lookInput = new Vector2(_lookInputX, _lookInputY);
 
-        transform.Rotate(_lookInput * _lookSensitivity * Time.deltaTime);
+        _lookDirection += _lookInput;
+
+        transform.localRotation = Quaternion.AngleAxis(_lookDirection.x * _lookSensitivity, Vector3.up);
+        _playerCamera.transform.localRotation = Quaternion.AngleAxis(-_lookDirection.y * _lookSensitivity, Vector3.right);
+
     }
 }
