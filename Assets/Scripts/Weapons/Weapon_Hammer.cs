2ca5c3c216f90d6acc254169ae2d8ee1b6861feb
Onsekyl <kimi.leinolaPC@outlook.com>
2024-08-11 20:31:14 +0300

    URL comment for getting current anim name

--- Assets/Scripts/Weapons/Weapon_Hammer.cs
+++ Assets/Scripts/Weapons/Weapon_Hammer.cs
@@ -10,6 +10,7 @@ public class Weapon_Hammer : Weapon
     }
     protected override void Attack()
     {
+        // https://stackoverflow.com/questions/62417760/how-to-retrieve-the-current-animation-playing-from-animator
         if (animator.GetCurrentAnimatorClipInfo(0)[0].clip.name == "Idle_Hammer")
         {
             animator.SetTrigger("AttackHammerTrigger");
