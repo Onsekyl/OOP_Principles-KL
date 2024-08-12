d5f559141b19e8d1f7aec657a1d052bbef65f3fe
Onsekyl <kimi.leinolaPC@outlook.com>
2024-08-12 18:43:20 +0300

    Kind of big commit
    - Damage dealing functionality
    - Weapons have trigger colliders which activates on specified window in attack animation
    - Dummy receives damage from any weapon that derives from Weapon class and has the tag "Weapon"

--- Assets/Scripts/Dummy.cs
+++ Assets/Scripts/Dummy.cs
@@ -5,17 +5,52 @@ using UnityEngine;
 public class Dummy : MonoBehaviour
 {
     [SerializeField]
-    private Animator _dummyAnimator;
+    private Animator _dummyAnim;
     [SerializeField]
-    private AnimationClip[] _dummyAnimClips;
+    private int _dummyHealth = 100;
 
     private void Start()
     {
-        
+
     }
 
     private void Update()
     {
-        
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Weapon"))
+        {
+            ReceiveDamage(other.GetComponent<Weapon>().damage);
+        }
+    }
+
+    public void ReceiveDamage(int damage)
+    {
+        _dummyHealth -= damage;
+
+        Debug.Log(damage + " damage received!");
+
+        if (_dummyHealth <= 0)
+        {
+            Die();
+        }
+
+        if (_dummyAnim != null)
+        {
+            // https://docs.unity3d.com/ScriptReference/Animator.Play.html
+            _dummyAnim.Play("Base Layer.pushed", 0, 0);
+        }
+        else
+        {
+            Debug.Log("Animator not assigned!");
+        }
+    }
+
+    private void Die()
+    {
+
     }
 }
